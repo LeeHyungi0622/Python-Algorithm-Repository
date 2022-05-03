cae9bd164301248b689bcbcb329622496b9aadc1
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-05-03 21:05:42 +0900

    feat: programmers 00번 신고 결과 받기 문제 풀기

---
+++ programmers_00.py
@@ -0,0 +1,38 @@
+def solution(id_list, report, k):
+    answer = []
+
+    report_lst = []
+    for i in report:
+        report_lst.append(i.split(' '))
+
+    report_dict = dict([(key, []) for key in id_list])
+
+    for i in report_lst:
+        if not i[1] in report_dict[i[0]]:
+            report_dict[i[0]].append(i[1])
+
+    reported_person_lst = []
+    for _, v in report_dict.items():
+        reported_person_lst.append(v)
+
+    reported_person_lst_flatten = sum(reported_person_lst, [])
+
+    reported_person_num = dict()
+
+    for item in reported_person_lst_flatten:
+        reported_person_num[item] = reported_person_num.get(item, 0) + 1
+
+    confirmed_person = []
+    for name, v in reported_person_num.items():
+        if v >= k:
+            confirmed_person.append(name)
+
+    for k, v in report_dict.items():
+        if v == confirmed_person:
+            answer.append(len(confirmed_person))
+        elif bool(set(confirmed_person).intersection(set(v))):
+            answer.append(len(set(confirmed_person).intersection(set(v))))
+        else:
+            answer.append(0)
+
+    return answer
