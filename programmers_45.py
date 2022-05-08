c887ccd03909b5e5ca251ad832d348f2fb793662
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-05-08 16:41:11 +0900

    feat: 프로그래머스 45번 제일 작은 수 제거하기 문제 풀이(레벨 1)

---
+++ programmers_45.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    answer = []
+
+    # -1을 배열에 채워서 반환하도록 처리(길이가 1이하인 경우 - 단일요소)
+    if len(arr) <= 1:
+        answer.append(-1)
+        return answer
+    else:
+        del arr[arr.index(min(arr))]
+        return arr
+
+
+arr = [4, 3, 2, 1]
+
+print(solution(arr))
